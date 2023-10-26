6d3dde10fe47f5c5982365c57ddc04fa311e5442
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-10-26 22:50:04 +0200

    derived state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
-import './App.css'
+import './App.css';
 import Box from './components/Box';
 import Boxes from './data/Boxes';
 import { useState } from 'react';
 
-function App() {
-  const [square,setSquare] = useState(Boxes);
+export default function App() {
+  const [square, setSquare] = useState(Boxes);
 
-  
-
+  function toggle(id) {
+    setSquare((prevSquare) => {
+      const newSquare = [];
+      for (let i = 0; i < prevSquare.length; i++) {
+        const currentSquare = prevSquare[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquare.push(updatedSquare);
+        } else {
+          newSquare.push(currentSquare);
+        }
+      }
+      return newSquare;
+    });
+  }
 
   return (
     <>
-    {
-      square.map((square)=>{
-        return(
-          <Box 
-            key={square.id} 
-            on={square.on} 
-        />
-        )
-      })
-    }
-        
+      {square.map((box) => {
+        return (
+          <Box
+            key={box.id}
+            on={box.on}
+            id={box.id}
+            toggle={toggle}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
-
-export default App
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
 
 export default function Box(props) {
-    const [on,setOn]= useState(props.on);
     const styles = {
-        backgroundColor: on ? "#222222" : "transparent"
+        backgroundColor: props.on ? "#222222" : "transparent"
     }
 
-    function Toggle() {
-        setOn(prevOn => !prevOn)
-    }
+   
     
     return (
         <div 
             className="box"
-            onClick={() => Toggle()}
+            onClick={() => props.toggle(props.id)}
             style={styles} 
         >
         </div>
